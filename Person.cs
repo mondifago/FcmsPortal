0f66fa95f34bef5f4991657d0d0290506e159db3
ugochukwumaduakor <umaduakor@hotmail.com>
2024-09-03 06:02:15 +1000

    renamed property Address to HomeAddress

--- Person.cs
+++ Person.cs
@@ -6,7 +6,7 @@
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
-        public Address Address { get; set; }
+        public Address HomeAddress { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public bool IsActive { get; set; }            // Active status of the staff member
