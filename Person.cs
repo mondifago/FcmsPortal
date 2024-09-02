6f9701ebe068cd0b0a5075faca381937c0de16fd
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-09-02 22:47:20 +1000

    Created Person class

---
+++ Person.cs
@@ -0,0 +1,14 @@
+﻿namespace FcmsPortal
+{
+    public class Person
+    {
+        public byte[] ProfilePicture { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public Address Address { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsActive { get; set; }            // Active status of the staff member
+    }
+}
