1f462aa851ffcc7486be677c5bf8daee32ed5a2d
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-09-03 14:17:16 +1000

    Guardian class added

---
+++ Guardian.cs
@@ -0,0 +1,38 @@
+﻿namespace FcmsPortal
+{
+    public class Guardian
+    {
+        private Person _theGuardian;
+
+        public Person TheGuardian
+        {
+            get { return _theGuardian; }
+            set { _theGuardian = value; }
+        }
+
+        private string _relationshipToStudent;
+
+        public string RelationshipToStudent
+        {
+            get { return _relationshipToStudent; }
+            set { _relationshipToStudent = value; }
+        }
+
+        private List<int> _childrenIds;
+
+        public List<int> ChildrenIds
+        {
+            get { return _childrenIds; }
+            set { _childrenIds = value; }
+        }
+
+        private string _occupation;
+
+        public string Occupation
+        {
+            get { return _occupation; }
+            set { _occupation = value; }
+        }
+
+    }
+}
--- Person.cs
+++ Person.cs
@@ -2,13 +2,60 @@
 {
     public class Person
     {
-        public byte[] ProfilePicture { get; set; }
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
-        public Address HomeAddress { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public bool IsActive { get; set; }            // Active status of the staff member
+        private byte[] _profilePicture;
+        public byte[] ProfilePicture
+        {
+            get { return _profilePicture; }
+            set { _profilePicture = value; }
+        }
+
+        private string _firstName;
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        private string _middleName;
+        public string MiddleName
+        {
+            get { return _middleName; }
+            set { _middleName = value; }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        private Address _homeAddress;
+        public Address HomeAddress
+        {
+            get { return _homeAddress; }
+            set { _homeAddress = value; }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        private string _phoneNumber;
+        public string PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set { _phoneNumber = value; }
+        }
+
+        private bool _isActive;
+        public bool IsActive
+        {
+            get { return _isActive; }
+            set { _isActive = value; }
+        }
     }
 }
