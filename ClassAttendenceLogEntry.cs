6e4f9d4ec2a0ec4e078cecd2ec8e776085b292c6
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-09-25 23:03:49 +1000

    ClassAttendenceLogEntry created

---
+++ ClassAttendenceLogEntry.cs
@@ -0,0 +1,10 @@
+﻿namespace FcmsPortal
+{
+    public class ClassAttendenceLogEntry
+    {
+        public int Id { get; set; }
+        public Person Teacher { get; set; }
+        public List<Person> Atendees { get; set; } = new();
+        public DateTime TimeStamp { get; set; }
+    }
+}
