705a10aba24f685969ae771205959831d05c8443
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-09-29 05:24:19 +1000

    Class School craeted

---
+++ School.cs
@@ -0,0 +1,10 @@
+﻿namespace FcmsPortal
+{
+    public class School
+    {
+        public List<Person> Staff { get; set; } = new();
+        public List<Person> Students { get; set; } = new();
+        public List<LearningPath> LearningPaths { get; set; } = new();
+        public List<Grade> Grades { get; set; } = new();
+    }
+}
