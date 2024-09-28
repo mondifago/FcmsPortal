4363096045d52b070f3c4eb94cb04d42f860c84b
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-09-29 03:23:25 +1000

    Created ScheduleEntry class to handle creating and deleting of class schedules

---
+++ ScheduleEntry.cs
@@ -0,0 +1,9 @@
+﻿namespace FcmsPortal
+{
+    public class ScheduleEntry
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public ClassSession ClassSession { get; set; }
+    }
+}
