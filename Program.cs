067ce7cff98ce98f6da4d116ccff9d3b96775bec
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-10-02 23:31:06 +1000

    Created an instance of a school with name and address

--- Program.cs
+++ Program.cs
@@ -10,17 +10,34 @@
         /// create an admin staff
         /// </summary>
         /// <param name="args"></param>
+        /// 
+
         static void Main(string[] args)
         {
-            /*var school = new School();
+            // create a school with name and address
+            School fcmSchool = new School
+            {
+                Name = "FCM School",
+                SchoolAddress = new Address
+                {
+                    Street = "120 City Road",
+                    City = "Asaba",
+                    State = "Delta State",
+                    PostalCode = "P.O.Box 150",
+                    Country = "Nigeria"
+                }
+            };
 
+
+
+            /*
             //create one teacher one student
 
-            var teacher = new Person { FirstName = "John", MiddleName = "Paul", LastName = "Kent" };
+            var teacher = new Staff { FirstName = "John", MiddleName = "Paul", LastName = "Kent" };
             var student1 = new Person { FirstName = "Scott", MiddleName = "Silas", LastName = "Anthony" };
 
-            school.Staff.Add(teacher);
-            school.Students.Add(student1);
+            fcmSchool.Staff.Add(teacher);
+            fcmSchool.Students.Add(student1);
 
 
             //create a class session biology class for babies:
