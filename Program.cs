8707442bc72bea20c2cab774868a28ca9943864c
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-10-03 00:41:21 +1000

    Created a primary 3 student and added him to list of school's students

--- Program.cs
+++ Program.cs
@@ -41,12 +41,31 @@
                     LastName = "Smith",
                     Sex = Gender.Male,
                     EducationLevel = EducationLevel.Primary,
+                    ClassLevel = "Primary 3"
                 }
             };
 
             // Add the mathematics teacher to the school's staff list
             fcmSchool.Staff.Add(mathTeacher);
 
+            //create a primary 3 student
+            Student student1 = new Student
+            {
+                StudentId = 304,
+                GuardianInfo = new Guardian { TheGuardian = { FirstName = "Adam", LastName = "Mark" } },
+                TheStudent = new Person
+                {
+                    FirstName = "Lucky",
+                    MiddleName = "Steve",
+                    LastName = "Mark",
+                    Sex = Gender.Male,
+                    EducationLevel = EducationLevel.Primary,
+                    ClassLevel = "Primary 3"
+                }
+            };
+
+            // Add the student to the school's student list
+            fcmSchool.Students.Add(student1);
 
 
 
