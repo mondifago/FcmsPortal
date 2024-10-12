4d58f55f5fde02a1b80224817e493f6d72ea7fdd
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-10-13 02:22:40 +1100

    JuniorCollege level created

---
+++ JuniorCollege.cs
@@ -0,0 +1,18 @@
+﻿namespace FcmsPortal
+{
+    public class JuniorCollege : ClassLevel
+    {
+        public enum Classes
+        {
+            Jss1,
+            Jss2,
+            Jss3
+        }
+        public Classes GetClass { get; set; }
+        public override string GetLevelName()
+        {
+            return GetClass.ToString();
+
+        }
+    }
+}
