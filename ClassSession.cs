c5ff451f25aec741e2248b94fbbeab28597cd5f6
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-10-14 02:02:52 +1100

    modified classlevel property in classsession and learningpath classes

--- ClassSession.cs
+++ ClassSession.cs
@@ -3,11 +3,11 @@
     public class ClassSession
     {
         public int ID;
-        public string Name { get; set; }
+        public Course Name { get; set; }
         public string Topic { get; set; }
         public string Description { get; set; }
         public EducationLevel EducationLevel { get; set; }
-        public string ClassLevel { get; set; }
+        public ClassLevel ClassLevel { get; set; }
         public int Semester { get; set; }
         public Staff Teacher { get; set; }
         public List<Student> Students { get; set; } = new List<Student>();
--- LearningPath.cs
+++ LearningPath.cs
@@ -3,9 +3,9 @@
     public class LearningPath
     {
         public int Id { get; set; }
-        public string Name { get; set; } //e.g Biology 
+        public Course Name { get; set; } //e.g Biology 
         public EducationLevel EducationLevel { get; set; }
-        public string ClassLevel { get; set; }
+        public ClassLevel ClassLevel { get; set; }
         public int Semester { get; set; }
         public List<ClassSession> ClassInfos { get; set; } = new List<ClassSession>();
         public List<ScheduleEntry> Schedule { get; set; } = new List<ScheduleEntry>();
