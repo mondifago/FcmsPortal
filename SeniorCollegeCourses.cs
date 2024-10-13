76fc47e092fa54a45ff2bb020a12086b91f5f56a
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-10-14 03:13:52 +1100

    created SeniorCollegeCourses class

---
+++ SeniorCollegeCourses.cs
@@ -0,0 +1,29 @@
+﻿namespace FcmsPortal
+{
+    public class SeniorCollegeCourses : Course
+    {
+        public enum Course
+        {
+            Mathematics,
+            EnglishLanguage,
+            LiteratureInEnglish,
+            Biology,
+            Chemistry,
+            Physics,
+            Economics,
+            Commerce,
+            FinancialAccounting,
+            CivicEducation,
+            Crs,
+            DataProcessing,
+            Geography,
+            Government,
+            AgriculturalScience
+        }
+        public Course GetCourse { get; set; }
+        public override string GetCourseName()
+        {
+            return GetCourse.ToString();
+        }
+    }
+}
