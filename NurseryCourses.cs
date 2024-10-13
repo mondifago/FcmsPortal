195c0f8c2c54a4da7a2230646e728220c492b383
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-10-14 03:16:57 +1100

    created NurseryCourses class

---
+++ NurseryCourses.cs
@@ -0,0 +1,24 @@
+﻿namespace FcmsPortal
+{
+    public class NurseryCourses : Course
+    {
+        public enum Course
+        {
+            Mathematics,
+            EnglishLanguage,
+            GeneralScience,
+            SocialHabit,
+            CreativeArt,
+            PhysicalandHealthHabit,
+            Crs,
+            Rhymes,
+            Phonics,
+            Handwriting
+        }
+        public Course GetCourse { get; set; }
+        public override string GetCourseName()
+        {
+            return GetCourse.ToString();
+        }
+    }
+}
