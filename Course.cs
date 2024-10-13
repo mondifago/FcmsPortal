2ea6924bf192933d8341ad82e847fcd45c189816
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-10-14 02:31:45 +1100

    created JuniorCollegeCourses sub class

--- Course.cs
+++ Course.cs
@@ -1,21 +1,7 @@
 ﻿namespace FcmsPortal
 {
-    public class Course
+    public abstract class Course
     {
-        private int _courseId;
-
-        public int CourseId
-        {
-            get { return _courseId; }
-            set { _courseId = value; }
-        }
-
-        private string _name;
-
-        public string Name
-        {
-            get { return _name; }
-            set { _name = value; }
-        }
+        public abstract string GetCourseName();
     }
 }
---
+++ JuniorCollegeCourses.cs
@@ -0,0 +1,24 @@
+﻿namespace FcmsPortal
+{
+    public class JuniorCollegeCourses : Course
+    {
+        public enum Course
+        {
+            Mathematics,
+            EnglishLanguage,
+            PreVocationalStudies,
+            NationalValues,
+            CulturalAndCreativeArt,
+            BasicScienceAndTechnology,
+            BusinessStudies,
+            Crs,
+            History,
+            French
+        }
+        public Course GetCourse { get; set; }
+        public override string GetCourseName()
+        {
+            return GetCourse.ToString();
+        }
+    }
+}
