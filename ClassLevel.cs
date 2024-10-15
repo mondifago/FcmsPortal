d42872f6b81fd913551711e3c2b5f318e705378b
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-10-16 03:30:57 +1100

    turn ClassLevel class to one enum containing all class levels

--- ClassLevel.cs
+++ ClassLevel.cs
@@ -1,7 +1,22 @@
 ﻿namespace FcmsPortal
 {
-    public abstract class ClassLevel
+    public enum ClassLevel
     {
-        public abstract string GetLevelName();
+        KG_Daycare,
+        KG_KG_PlayGroup,
+        KG_PreNursery,
+        KG_Nursery,
+        Primary1,
+        Primary2,
+        Primary3,
+        Primary4,
+        Primary5,
+        Primary6,
+        JuniorCollege1,
+        JuniorCollege2,
+        JuniorCollege3,
+        SeniorCollege1,
+        SeniorCollege2,
+        SeniorCollege3
     }
 }
