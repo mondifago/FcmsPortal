20b13e6b5838f3f30f5a501b0f24ff0cb5a7d8ab
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-10-18 06:15:43 +1100

    Grade class, GradingFormular and scores are amended to their simplest form

--- Grade.cs
+++ Grade.cs
@@ -6,13 +6,5 @@
         public Dictionary<Student, Score> Scores { get; private set; } = new Dictionary<Student, Score>();
         public GradingFormula Formula { get; private set; }
 
-        public Grade(int classSessionId, GradingFormula formula)
-        {
-            ClassSessionId = classSessionId;
-            Formula = formula;
-        }
-
-
-
     }
 }
