65a12eaf4c3f3c89fc6f045b205c3b74fbebec5d
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-10-18 06:34:39 +1100

    Lessonnote, homeWork and TeachersRemark properties added to ClassSession Class

--- ClassSession.cs
+++ ClassSession.cs
@@ -6,9 +6,12 @@
         public string Course { get; set; }
         public string Topic { get; set; }
         public string Description { get; set; }
+        public string LessonNote { get; set; }
+        public string HomeWork { get; set; }
         public Staff Teacher { get; set; }
         public List<ClassAttendanceLogEntry> AttendanceLog { get; set; } = new List<ClassAttendanceLogEntry>();
         public Grade Grade { get; set; }
+        public string TeacherRemarks { get; set; }
     }
 
 
