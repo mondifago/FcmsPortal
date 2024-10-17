62a07d4525481389d45fbe68a04aa76e11606218
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-10-18 03:51:38 +1100

    amend Grade class

--- Grade.cs
+++ Grade.cs
@@ -2,10 +2,12 @@
 {
     public class Grade
     {
-        public int Id { get; set; }
-        public Course Course { get; set; }
-        public Staff GradingTeacher { get; set; }
-        public Student Student { get; set; }
-        public int QuizScore { get; set; }
+        public Student Student { get; private set; }
+        public Course Course { get; private set; }
+        public List<double> HomeworkScores { get; private set; } = new List<double>();
+        public List<double> QuizScores { get; private set; } = new List<double>();
+        public double AttendanceScore { get; private set; }
+        public double ExamScore { get; private set; }
+        public GradingFormula Formula { get; private set; }
     }
 }
