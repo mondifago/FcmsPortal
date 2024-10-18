6eabb4adbfcce60327c654e340996eaeed78c4a2
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-10-19 03:57:53 +1100

    Replaced List of LaerningPath property with Calender in Class School

--- School.cs
+++ School.cs
@@ -7,7 +7,7 @@
         public List<Staff> Staff { get; set; } = new();
         public List<Student> Students { get; set; } = new();
         public List<Guardian> Guardians { get; set; } = new();
-        public List<LearningPath> LearningPaths { get; set; } = new();
+        public SchoolCalendar SchoolCalendar { get; set; }
         public List<CourseGrade> Grades { get; set; } = new();
     }
 }
