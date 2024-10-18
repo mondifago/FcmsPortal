1fa951ab26e58aaa9c9a26e82b55eaf978c27f5f
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-10-19 03:56:09 +1100

    Created SchoolCalender Class

---
+++ SchoolCalendar.cs
@@ -0,0 +1,9 @@
+﻿namespace FcmsPortal
+{
+    public class SchoolCalendar
+    {
+        public int Id { get; set; }
+        public string AcademicYear { get; set; }
+        public List<LearningPath> LearningPaths { get; set; } = new List<LearningPath>();
+    }
+}
