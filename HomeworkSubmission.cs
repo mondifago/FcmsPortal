df6262024104f3d12d2fbc0ac531034e280e2775
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-12-04 23:06:03 +1100

    created Class HomeworkSubmission

---
+++ HomeworkSubmission.cs
@@ -0,0 +1,10 @@
+﻿namespace FcmsPortal
+{
+    public class HomeworkSubmission
+    {
+        public int Id { get; set; }
+        public Student Student { get; set; }
+        public string Answer { get; set; }
+        public DateTime SubmissionDate { get; set; }
+    }
+}
