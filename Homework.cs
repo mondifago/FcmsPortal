228bc8835f73aca5ad2ba628130df39a7181d2b9
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-12-05 00:54:54 +1100

    homework class created

---
+++ Homework.cs
@@ -0,0 +1,14 @@
+﻿namespace FcmsPortal
+{
+    public class Homework
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime AssignedDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public ClassSession ClassSession { get; set; } // Reference to the session
+        public List<HomeworkSubmission> Submissions { get; set; } = new List<HomeworkSubmission>();
+        public List<DiscussionThread> Discussions { get; set; } = new();
+    }
+}
