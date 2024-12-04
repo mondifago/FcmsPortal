d4dfeea91e6ac5c8a84b87cd69263d7f1ddc2cc9
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-12-05 01:12:23 +1100

    DiscussionThread Class created to support collaboration in home work excution

---
+++ DiscussionThread.cs
@@ -0,0 +1,11 @@
+﻿namespace FcmsPortal
+{
+    public class DiscussionThread
+    {
+        public int Id { get; set; }
+        public Student Student { get; set; }
+        public string Comment { get; set; } // The question or comment
+        public DateTime Timestamp { get; set; }
+        public List<TeacherReply> Replies { get; set; } = new(); // Replies from the teacher
+    }
+}
