d00691031b435ad4b68cfc8c108dc4c4d9792592
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-12-05 01:05:00 +1100

    homework property replace with homeworkdatail as an object for all homework concerns

--- ClassSession.cs
+++ ClassSession.cs
@@ -7,9 +7,9 @@
         public string Topic { get; set; }
         public string Description { get; set; }
         public string LessonNote { get; set; }
-        public string HomeWork { get; set; }
         public Staff Teacher { get; set; }
         public List<ClassAttendanceLogEntry> AttendanceLog { get; set; } = new List<ClassAttendanceLogEntry>();
+        public Homework HomeworkDetails { get; set; }
         public string TeacherRemarks { get; set; }
     }
 }
