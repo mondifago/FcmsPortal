69d786b8dd381315fcd4292dd6aa0c4e7150f29b
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-12-05 00:57:28 +1100

    HomeworkResponse class created

---
+++ HomeworkResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace FcmsPortal
+{
+    public class HomeworkResponse
+    {
+        public int Id { get; set; }
+        public Homework Homework { get; set; }
+        public Staff Teacher { get; set; }
+        public string GeneralFeedback { get; set; } // Teacher's general comments/answers for the class
+        public DateTime ReviewDate { get; set; }
+    }
+
+}
