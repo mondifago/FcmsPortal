1a084f5f34d4b30ae28626710183fac8dab37ef5
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2024-12-07 13:22:22 +1100

    Class DisscusionThread modified for anyone to use

--- DiscussionThread.cs
+++ DiscussionThread.cs
@@ -3,9 +3,8 @@
     public class DiscussionThread
     {
         public int Id { get; set; }
-        public Student Student { get; set; }
+        public Person Author { get; set; }
         public string Comment { get; set; } // The question or comment
         public DateTime Timestamp { get; set; }
-        public List<TeacherReply> Replies { get; set; } = new(); // Replies from the teacher
     }
 }
