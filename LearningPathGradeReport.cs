9d4306401af47b7a27c40b96c8c0d118844cf48c
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-02-06 23:54:18 +1100

    fix #84 Class LeraningPathGradeReport

---
+++ LearningPathGradeReport.cs
@@ -0,0 +1,20 @@
+﻿using FcmsPortal.Enums;
+
+namespace FcmsPortal
+{
+    public class LearningPathGradeReport
+    {
+        public int Id { get; set; }
+        public LearningPath LearningPath { get; set; }
+        public Semester Semester { get; set; }
+        public Dictionary<Student, double> StudentSemesterGrades { get; set; } = new();
+        public List<StudentGradeSummary> RankedStudents { get; set; } = new();
+        public bool IsFinalized { get; set; }
+    }
+
+    public class StudentGradeSummary
+    {
+        public Student Student { get; set; }
+        public double SemesterOverallGrade { get; set; }
+    }
+}
