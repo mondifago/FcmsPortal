fc10ce35627b4dd284e67f8fe5202b620bbbb19c
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-02-24 20:42:25 +1100

    Added validation annotations to Address Model

--- Models/Address.cs
+++ Models/Address.cs
@@ -1,15 +1,26 @@
 ﻿using FcmsPortal.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace FcmsPortal.Models
 {
     public class Address
     {
         public int Id { get; set; }
-        public string Street { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string PostalCode { get; set; }
-        public string Country { get; set; }
+
+        [Required(ErrorMessage = "Street is required.")]
+        public string Street { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "City is required.")]
+        public string City { get; set; } = string.Empty;
+
+        public string? State { get; set; }
+
+        [StringLength(10, MinimumLength = 5, ErrorMessage = "Postal Code must be between 5 and 10 characters.")]
+        public string? PostalCode { get; set; }
+
+        [Required(ErrorMessage = "Country is required.")]
+        public string Country { get; set; } = string.Empty;
+
         public AddressType AddressType { get; set; }
     }
 }
