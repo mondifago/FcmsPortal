bd9fb30f28280d72662780b5f14dd3ff144b24a7
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-02-25 02:30:59 +1100

    validation annotation applied to payment class

--- Models/Payment.cs
+++ Models/Payment.cs
@@ -1,13 +1,23 @@
 ﻿using FcmsPortal.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace FcmsPortal.Models
 {
     public class Payment
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Amount is required.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than 0.")]
         public double Amount { get; set; }
+
+        [Required(ErrorMessage = "Date is required.")]
         public DateTime Date { get; set; }
+
         public PaymentMethod PaymentMethod { get; set; }
+
+        [Required(ErrorMessage = "Reference is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Reference must be a positive number.")]
         public int Reference { get; set; }
     }
 }
