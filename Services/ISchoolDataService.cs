f7e3aec31f9a67b53292928498fd8341d9af7f38
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-03-16 00:05:51 +1100

    Added School.razor

--- Services/ISchoolDataService.cs
+++ Services/ISchoolDataService.cs
@@ -11,6 +11,8 @@ namespace FcmsPortal.Services
         IEnumerable<Guardian> GetGuardians();
         IEnumerable<Student> GetStudentsByClassLevel(ClassLevel classLevel);
         IEnumerable<Staff> GetTeachersByEducationLevel(EducationLevel educationLevel);
+        Guardian GetGuardianById(int id);
+        void UpdateGuardian(Guardian guardian);
     }
 
     public class SchoolDataService : ISchoolDataService
@@ -40,6 +42,46 @@ namespace FcmsPortal.Services
             return _school.Staff.Where(s =>
                 s.JobRole == JobRole.Teacher &&
                 s.Person.EducationLevel == educationLevel);
+        }
+
+        public Guardian GetGuardianById(int id)
+        {
+            return _school.Guardians.FirstOrDefault(g => g.Id == id);
+        }
+
+        public void UpdateGuardian(Guardian guardian)
+        {
+            var existingGuardian = _school.Guardians.FirstOrDefault(g => g.Id == guardian.Id);
+            if (existingGuardian != null)
+            {
+                // Copy properties from updated guardian to existing one
+                existingGuardian.Person.FirstName = guardian.Person.FirstName;
+                existingGuardian.Person.MiddleName = guardian.Person.MiddleName;
+                existingGuardian.Person.LastName = guardian.Person.LastName;
+                existingGuardian.Occupation = guardian.Occupation;
+                existingGuardian.RelationshipToStudent = guardian.RelationshipToStudent;
+                // Copy other properties as needed
+            }
+        }
+
+        public void UpdateSchool(School updatedSchool)
+        {
+            // Update the properties of the existing school
+            _school.Name = updatedSchool.Name;
+            _school.LogoUrl = updatedSchool.LogoUrl;
+            _school.Email = updatedSchool.Email;
+            _school.PhoneNumber = updatedSchool.PhoneNumber;
+            _school.WebsiteUrl = updatedSchool.WebsiteUrl;
+
+            // Update address
+            if (_school.Address != null && updatedSchool.Address != null)
+            {
+                _school.Address.Street = updatedSchool.Address.Street;
+                _school.Address.City = updatedSchool.Address.City;
+                _school.Address.State = updatedSchool.Address.State;
+                _school.Address.PostalCode = updatedSchool.Address.PostalCode;
+                _school.Address.Country = updatedSchool.Address.Country;
+            }
         }
     }
 }
