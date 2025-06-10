2a2bface828064a42772786f8c00a51caee92c56
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-06-10 18:25:39 +1000

    fix #153 Create Class Session Report Entry View Model

---
+++ Models/ClassSessionReport.cs
@@ -0,0 +1,12 @@
+﻿namespace FcmsPortal.Models
+{
+    public class ClassSessionReport
+    {
+        public int ClassSessionId { get; set; }
+        public string LearningPathName { get; set; }
+        public string Course { get; set; }
+        public string Topic { get; set; }
+        public string SubmittedBy { get; set; }
+        public DateTime TimeSubmitted { get; set; } = DateTime.Now;
+    }
+}
