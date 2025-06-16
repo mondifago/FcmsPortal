3a1958e4beee465169226d0a4b6673be95df9825
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-06-16 13:44:18 +1000

    added CourseGradeConfiguration Model

---
+++ Models/CourseGradingConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace FcmsPortal.Models
+{
+    public class CourseGradingConfiguration
+    {
+        public int Id { get; set; }
+        public string Course { get; set; }
+        public int LearningPathId { get; set; }
+        public double HomeworkWeightPercentage { get; set; }
+        public double QuizWeightPercentage { get; set; }
+        public double FinalExamWeightPercentage { get; set; }
+        public LearningPath LearningPath { get; set; }
+    }
+}
