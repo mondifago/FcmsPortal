4293b033c2f2931a7dd8edf3a476c7a33b7c62ad
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-06-24 05:13:08 +1000

    Create GradeReport Model for Grade Report submissiom

---
+++ Models/GradesReport.cs
@@ -0,0 +1,14 @@
+﻿using FcmsPortal.Enums;
+
+namespace FcmsPortal.Models
+{
+    public class GradesReport
+    {
+        public int LearningPathId { get; set; }
+        public string LearningPathName { get; set; }
+        public DateTime DateSubmitted { get; set; }
+        public string SubmittedBy { get; set; }
+        public int NumberOfStudents { get; set; }
+        public PrincipalApprovalStatus Status { get; set; } = PrincipalApprovalStatus.Pending;
+    }
+}
