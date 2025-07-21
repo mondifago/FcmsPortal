41b87c6df3173c743b60ec212f8d5bc54b4e4bd3
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-07-21 11:53:12 +1000

    fix #212 Complete Both process and form validation for Guardian Entity

--- Models/Guardian.cs
+++ Models/Guardian.cs
@@ -6,13 +6,13 @@ namespace FcmsPortal.Models
     public class Guardian
     {
         public int Id { get; set; }
-        [Required]
         public Person Person { get; set; } = new Person();
 
         [Required(ErrorMessage = "RelationShip with Student is Required")]
         public Relationship RelationshipToStudent { get; set; }
 
+        [Required(ErrorMessage = "Occupation is Required")]
         public string Occupation { get; set; } = string.Empty;
-        public List<Student> Wards { get; set; } = new();
+        public List<Student> Wards { get; set; } = new List<Student>();
     }
 }
