a4fa7f20d04394afe9dc98bb1bb82243b6d95023
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-09-12 23:29:39 +1000

    fix #275 Create New Grade Archiving Class Model

---
+++ Models/ArchivedPaymentDetail.cs
@@ -0,0 +1,12 @@
+﻿using FcmsPortal.Enums;
+
+namespace FcmsPortal.Models
+{
+    public class ArchivedPaymentDetail
+    {
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public int Reference { get; set; }
+    }
+}
---
+++ Models/ArchivedStudentPayment.cs
@@ -0,0 +1,24 @@
+﻿using FcmsPortal.Enums;
+
+namespace FcmsPortal.Models
+{
+    public class ArchivedStudentPayment
+    {
+        public int Id { get; set; }
+        public int OriginalStudentId { get; set; }
+        public string StudentName { get; set; } = string.Empty;
+        public int OriginalLearningPathId { get; set; }
+        public string LearningPathName { get; set; } = string.Empty;
+        public EducationLevel EducationLevel { get; set; }
+        public ClassLevel ClassLevel { get; set; }
+        public Semester Semester { get; set; }
+        public string AcademicYear { get; set; } = string.Empty;
+        public double TotalFees { get; set; }
+        public double TotalPaid { get; set; }
+        public double OutstandingBalance { get; set; }
+        public double PaymentCompletionRate { get; set; }
+        public double TimelyCompletionRate { get; set; }
+        public string PaymentDetailsJson { get; set; } = string.Empty;
+        public DateTime ArchivedDate { get; set; }
+    }
+}
