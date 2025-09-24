f6ae68a7cdfb02c69d66e58a98367f760dffd229
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-09-24 18:59:58 +1000

    fix #277 Create AttendanceArchive Class Model

---
+++ Models/AttendanceArchive.cs
@@ -0,0 +1,20 @@
+﻿using FcmsPortal.Enums;
+
+namespace FcmsPortal.Models
+{
+    public class AttendanceArchive
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public string StudentName { get; set; } = string.Empty;
+        public int LearningPathId { get; set; }
+        public string LearningPathName { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public bool IsPresent { get; set; }
+        public DateTime AcademicYearStart { get; set; }
+        public Semester Semester { get; set; }
+        public EducationLevel EducationLevel { get; set; }
+        public ClassLevel ClassLevel { get; set; }
+        public DateTime ArchivedDate { get; set; }
+    }
+}
