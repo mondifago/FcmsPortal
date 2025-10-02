d922885abf6c64ce2eb1b8091dc850936767116f
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-10-03 00:05:43 +1000

    fix #282 Create ApplicationUser to inherit from IdentityUser

--- Models/Person.cs
+++ Models/Person.cs
@@ -1,10 +1,11 @@
 ﻿using FcmsPortal.Enums;
+using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FcmsPortal.Models
 {
-    public class Person
+    public class Person : IdentityUser
     {
         public int Id { get; set; }
         public string? ProfilePictureUrl { get; set; }
