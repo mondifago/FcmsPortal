b86655d361daf92a231ed8e66d002daf332d1d0c
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-11-29 13:54:23 +1100

    Create ArchivedCourseGrade

---
+++ Models/ArchivedCourseGrade.cs
@@ -0,0 +1,18 @@
+﻿namespace FcmsPortal.Models
+{
+    public class ArchivedCourseGrade
+    {
+        public int Id { get; set; }
+        public string Course { get; set; } = string.Empty;
+        public double HomeworkWeightPercentage { get; set; }
+        public double QuizWeightPercentage { get; set; }
+        public double FinalExamWeightPercentage { get; set; }
+        public double CumulatedHomeworkGrade { get; set; }
+        public double CumulatedQuizGrade { get; set; }
+        public double CumulatedExamGrade { get; set; }
+        public double TotalGrade { get; set; }
+        public string FinalGradeCode { get; set; } = string.Empty;
+        public List<ArchivedTestGrade> TestGrades { get; set; } = new();
+        public int ArchivedSemesterGradeId { get; set; }
+    }
+}
