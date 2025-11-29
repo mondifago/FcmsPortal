e974b4cf96aa057b48932a4ac398c9a3945d3863
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-11-29 13:53:54 +1100

    Create ArchivedTestGrade

---
+++ Models/ArchivedTestGrade.cs
@@ -0,0 +1,13 @@
+﻿namespace FcmsPortal.Models
+{
+    public class ArchivedTestGrade
+    {
+        public int Id { get; set; }
+
+        public string TestType { get; set; } = string.Empty;
+        public double Score { get; set; }
+        public double MaxScore { get; set; }
+        public DateTime DateRecorded { get; set; }
+        public int ArchivedCourseGradeId { get; set; }
+    }
+}
