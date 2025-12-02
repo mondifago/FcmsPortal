64654d7c277f7e60c90301e1b5071eb24a262e8e
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-12-03 01:52:16 +1100

    fix #389 Create ArchivedTestGrade

---
+++ Models/ArchivedTestGrade.cs
@@ -0,0 +1,18 @@
+﻿using FcmsPortal.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace FcmsPortal.Models
+{
+    public class ArchivedTestGrade
+    {
+        public int Id { get; set; }
+        public int ArchivedCourseGradeId { get; set; }
+        public ArchivedCourseGrade? ArchivedCourseGrade { get; set; }
+        [Range(0, 100)]
+        public double Score { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        public GradeType GradeType { get; set; }
+    }
+}
