ce4bbf2b5703ae6f364026aadb93994c4b4e672e
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-12-03 02:23:23 +1100

    fix #392 Create ArchivedLearningPathGrade

---
+++ Models/ArchivedLearningPathGrade.cs
@@ -0,0 +1,21 @@
+﻿using FcmsPortal.Enums;
+
+namespace FcmsPortal.Models
+{
+    public class ArchivedLearningPathGrade
+    {
+        public int Id { get; set; }
+        public int LearningPathId { get; set; }
+        public string LearningPathName { get; set; } = string.Empty;
+        public string AcademicYear { get; set; } = string.Empty;
+        public EducationLevel EducationLevel { get; set; }
+        public ClassLevel ClassLevel { get; set; }
+        public Semester Semester { get; set; }
+        public DateTime SemesterStartDate { get; set; }
+        public DateTime SemesterEndDate { get; set; }
+        public int TotalStudentsInPath { get; set; }
+        public double AverageClassGrade { get; set; }
+        public DateTime ArchivedDate { get; set; } = DateTime.Now;
+        public List<ArchivedStudentGrade> StudentGrades { get; set; } = new List<ArchivedStudentGrade>();
+    }
+}
