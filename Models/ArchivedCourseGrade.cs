fa4b1b09c2b2298a634ef1c91cda96b71a5685d3
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-12-03 02:22:16 +1100

    fix #390 Create ArchivedCourseGrade

---
+++ Models/ArchivedCourseGrade.cs
@@ -0,0 +1,19 @@
+﻿namespace FcmsPortal.Models
+{
+    public class ArchivedCourseGrade
+    {
+        public int Id { get; set; }
+        public int ArchivedStudentGradeId { get; set; }
+        public ArchivedStudentGrade? ArchivedStudentGrade { get; set; }
+        public string Course { get; set; } = string.Empty;
+        public double TotalGrade { get; set; }
+        public string FinalGradeCode { get; set; } = string.Empty;
+        public double HomeworkWeightPercentage { get; set; }
+        public double QuizWeightPercentage { get; set; }
+        public double FinalExamWeightPercentage { get; set; }
+        public double HomeworkAverage { get; set; }
+        public double QuizAverage { get; set; }
+        public double ExamAverage { get; set; }
+        public List<ArchivedTestGrade> TestGrades { get; set; } = new List<ArchivedTestGrade>();
+    }
+}
