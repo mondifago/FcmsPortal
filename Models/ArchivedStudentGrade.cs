94496c84b83b1897101bb0e55a7f3dce21b33b7e
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2025-12-03 19:45:07 +1100

    fix #397 Add more report card properties to ArchivedStudentGrade

--- Models/ArchivedStudentGrade.cs
+++ Models/ArchivedStudentGrade.cs
@@ -7,6 +7,11 @@
         public ArchivedLearningPathGrade? ArchivedLearningPathGrade { get; set; }
         public int StudentId { get; set; }
         public string StudentName { get; set; } = string.Empty;
+        public int StudentAge { get; set; }
+        public string StudentEmail { get; set; } = string.Empty;
+        public string GuardianName { get; set; } = string.Empty;
+        public string GuardianEmail { get; set; } = string.Empty;
+        public string GuardianPhoneNumber { get; set; } = string.Empty;
         public double SemesterOverallGrade { get; set; }
         public int StudentRank { get; set; }
         public double PromotionGrade { get; set; }
@@ -15,6 +20,9 @@
         public double AttendanceRate { get; set; }
         public bool IsPromoted { get; set; }
         public string PromotionStatus { get; set; } = string.Empty;
+        public double FirstSemesterGrade { get; set; }
+        public double SecondSemesterGrade { get; set; }
+        public double ThirdSemesterGrade { get; set; }
         public List<ArchivedCourseGrade> CourseGrades { get; set; } = new List<ArchivedCourseGrade>();
         public StudentReportCard? ArchivedReportCard { get; set; }
 
